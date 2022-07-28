f587c210b3a2bea45439bdcf03321d534c67f693
Reland Pigte <reland102088@gmail.com>
2022-07-28 10:29:38 +0800

    config

--- startup/config.js
+++ startup/config.js
@@ -3,6 +3,7 @@ const logger = require('../logger/logger')
 
 module.exports = function () {
   if (!config.get('jwtPrivateKey')) {
+    logger.log('info', 'Private key is not defined.')
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.')
   }
 }
