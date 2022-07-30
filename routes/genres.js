51e84ff98b287d5652f40ef60685eb9c02059bc2
Reland Pigte <reland102088@gmail.com>
2022-07-30 20:32:17 +0800

    Initial commit

--- routes/genres.js
+++ routes/genres.js
@@ -1,24 +1,28 @@
 const { Genre, validate } = require('../model/genre')
-const express = require('express')
-const router = express.Router()
+const _ = require('lodash')
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
 const validateObjectId = require('../middleware/validateObjectId')
 const validateGenre = require('../middleware/validate')
+const express = require('express')
+const router = express.Router()
 
 router.get('/', async (req, res) => {
-  res.send(await Genre.find().sort('name'))
+  const genre = await Genre.find().select().sort('name')
+  res.send(_.pick(genre, ['_id', 'name']))
 })
 
 router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findOne({ _id: req.params.id })
   if (!genre) return res.status(404).send('Genre not found!')
-  res.send(genre)
+  res.send(_.pick(genre, ['_id', 'name']))
 })
 
 router.post('/', [auth, admin, validateGenre(validate)], async (req, res) => {
   const genre = new Genre({ name: req.body.name })
-  res.send(await genre.save())
+  await genre.save()
+
+  res.send(_.pick(genre, ['_id', 'name']))
 })
 
 router.put(
@@ -31,14 +35,14 @@ router.put(
     })
     if (!genre) return res.status(404).send('Genre not found!')
 
-    res.send(genre)
+    res.send(_.pick(genre, ['_id', 'name']))
   },
 )
 
 router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
   const genre = await Genre.findByIdAndDelete(req.params.id)
   if (!genre) return res.status(404).send('Genre not found!')
-  res.send(genre)
+  res.send(_.pick(genre, ['_id', 'name']))
 })
 
 module.exports = router
