e4c924d160afcbdcc6c6656f20ef02fa61161196
Reland Pigte <reland102088@gmail.com>
2022-07-31 16:53:34 +0800

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/me', auth, async (req, res) => {
   res.send(user)
 })
 
-router.post('/', [auth, admin, validateUser(validate)], async (req, res) => {
+router.post('/', [validateUser(validate)], async (req, res) => {
   let user = await User.findOne({ email: req.body.email })
   if (user) return res.status(400).send('User already registered.')
 
